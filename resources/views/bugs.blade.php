63628dc5530b725f18b2a1f778ffd04b74ed4436
Vocal <vocalnight@protonmail.com>
2024-09-11 23:59:24 -0300

    Table working

--- resources/views/bugs.blade.php
+++ resources/views/bugs.blade.php
@@ -3,13 +3,21 @@
         Bugs
     </x-slot:heading>
 
-    <ul>
-        @foreach ($bugs as $bug )
-            <li>
-                <strong>{{ $bug['title']}} - {{$bug['AssignedTo']}}</strong>
-            </li>
-
-        @endforeach
-    </ul>
+    <table class="table-fixed border-collapse border-b border-slate-500 min-w-full">
+        <thead>
+            <tr>
+                <th class="border-b border-slate-600">Title</th>
+                <th class="border-b border-slate-600">Assigned To</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($bugs as $bug )
+                <tr class="border-b border-slate-600">
+                    <td class=""><strong>{{ $bug['title']}}</strong></td>
+                    <td><strong>{{$bug->assignedTo['name']}}</strong></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 
 </x-layout>
