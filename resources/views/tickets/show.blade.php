1d154f7232714258417feb652f537bd5be28d6be
Vocal <vocalnight@protonmail.com>
2024-09-24 20:13:12 -0300

    fixing css

--- resources/views/tickets/show.blade.php
+++ resources/views/tickets/show.blade.php
@@ -6,22 +6,16 @@
     </x-slot>
 
     <form>
-        <div class="space-y-12">
-            <div class="border-b border-gray-900/10 pb-12">
-                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
-                        <label for="title" class="block text-sm font-medium leading-6 dark:text-gray-200">Title</label>
-                        <div class="mt-2">
-                            <div
-                                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                <input type="text" name="title" id="title" autocomplete="title" value={{$bug->title}}
-                                    class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 px-3 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" disabled>
-                            </div>
-                        </div>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                    <div class="max-w-xl">
+                        <x-input-label for="title" :value="__('Title')" />
+                        <x-text-input id="title" name="title" type="text" class="mt-1 block w-full" required autofocus autocomplete="title" @disabled(true) value={{$bug->title}}/>
+                        <x-input-error class="mt-2" :messages="$errors->get('title')" />
                     </div>
                 </div>
-            </div>
-        </div>
 
         <div class="border-b border-gray-900/10 pb-12">
             <h2 class="text-base font-semibold leading-7 text-gray-900">Ticket Information</h2>
