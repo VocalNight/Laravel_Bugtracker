3735ca95cf2f306bef42393e27bd8c2af6f4bd11
Vocal <vocalnight@protonmail.com>
2024-09-24 22:33:25 -0300

    refactored show view to use new theme

--- resources/views/tickets/show.blade.php
+++ resources/views/tickets/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            Viewing Ticket: {{$bug->title}}
+            Viewing Ticket: {{ $bug->title }}
         </h2>
     </x-slot>
 
@@ -12,47 +12,56 @@
                 <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                     <div class="max-w-xl">
                         <x-input-label for="title" :value="__('Title')" />
-                        <x-text-input id="title" name="title" type="text" class="mt-1 block w-full" required autofocus autocomplete="title" @disabled(true) value={{$bug->title}}/>
+                        <x-text-input id="title" name="title" type="text" class="mt-1 block w-full" required
+                            autofocus autocomplete="title" :disabled="true" :value="old('title', $bug->title)" />
                         <x-input-error class="mt-2" :messages="$errors->get('title')" />
                     </div>
                 </div>
 
-        <div class="border-b border-gray-900/10 pb-12">
-            <h2 class="text-base font-semibold leading-7 text-gray-900">Ticket Information</h2>
+                <h2 class="text-base font-semibold leading-7 text-gray-50">Ticket Information</h2>
 
-            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                <div class="sm:col-span-3">
-                    <label for="description" class="block text-sm font-medium leading-6 text-gray-900">Description</label>
-                    <div class="mt-2">
-                        <textarea type="text" name="description" id="description"
-                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 px-2 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" disabled>{{$bug->description}}</textarea>
+                <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                    <div class="mt-1 flex-col gap-x-6">
+                        <div class="max-w-xl">
+                            <x-input-label for="description" :value="__('Description')" />
+                            <div class="pt-2">
+                                <textarea type="text" name="description" id="description" rows="10" cols="50"
+                                    class="block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">{{ $bug->description }}</textarea>
+                            </div>
+                        </div>
+
+                        <div class="max-w-xl pt-3">
+                            <x-input-label for="assigned" :value="__('Assigned To')" />
+
+                            <div class="mt-2">
+                                <select id="assigned" name="assigned" autocomplete="country-name"
+                                    class="block w-full py-1.5 text-gray-900 ring-1 ring-inset focus:ring-2 focus:ring-inset sm:max-w-xs sm:text-sm sm:leading-6 border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300  dark:focus:border-indigo-600 dark:focus:ring-indigo-600 rounded-md shadow-sm"
+                                    disabled>
+                                    <option value="{{ $bug->AssignedTo }}" selected>{{ $user->name }}
+                                    </option>
+                                </select>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                <div class="sm:col-span-3">
-                    <label for="assigned" class="block text-sm font-medium leading-6 text-gray-900">Assigned To</label>
-                    <div class="mt-2">
-                        <select id="assigned" name="assigned" autocomplete="country-name"
-                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6" disabled>
-                            <option value="{{$bug->AssignedTo}}" selected>{{$user->name}}</option>
-                        </select>
-                    </div>
-                </div>
             </div>
+
         </div>
+
     </form>
 
-   <div class="mt-6 flex items-center justify-between gap-x-6">
-    <div class="flex items-center">
-        <button form="delete-form" class="text-red-500 text-sm font-bold">Delete</button>
-    </div>
-    <div>
-        <x-button href="/tickets/{{$bug->id}}/edit">Edit Ticket</x-button>
+    <div class="mt-6 flex items-center justify-between gap-x-6">
+        <div class="flex items-center">
+            <button form="delete-form" class="text-red-500 text-sm font-bold">Delete</button>
+        </div>
+        <div>
+            <x-button href="/tickets/{{ $bug->id }}/edit">Edit Ticket</x-button>
+        </div>
+
     </div>
 
-</div>
-
-   <form method="POST" action="/tickets/{{$bug->id}}" id="delete-form" class="hidden">
+    <form method="POST" action="/tickets/{{ $bug->id }}" id="delete-form" class="hidden">
         @csrf
         @method('DELETE')
     </form>
