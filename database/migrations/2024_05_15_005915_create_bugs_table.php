40d340c916adb7c2d87dad3967afa789fee2ed84
Vocal <vocalnight@protonmail.com>
2024-10-13 01:53:25 -0300

    made sure both columns in tickets are constrained correctly

--- database/migrations/2024_05_15_005915_create_bugs_table.php
+++ database/migrations/2024_05_15_005915_create_bugs_table.php
@@ -18,8 +18,8 @@ return new class extends Migration
             $table->string('title');
             $table->string('description');
             //Make separated table for types of ticket/bug
-            $table->foreignIdFor(User::class, 'AssignedTo')->nullable()->onDelete('set null');
-            $table->foreignIdFor(User::class, 'CreatedBy');
+            $table->foreignIdFor(User::class, 'AssignedTo')->constrained('users')->cascadeOnDelete();
+            $table->foreignIdFor(User::class, 'CreatedBy')->constrained('users')->cascadeOnDelete();
             $table->foreignIdFor(Project::class, 'project')->constrained()->cascadeOnDelete();
         });
     }
